ef18600a81e833ba227180fc7f3750b58f9e9156
Diogo Madeira <diogomadeira10@gmail.com>
2024-06-10 20:21:20 +0100

    add login page

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,11 +1,12 @@
 import { SignInForm } from '@/features/auth/components/sign-in-form';
 import { getAuth } from '@/features/auth/queries/get-auth';
 import { getTranslations } from 'next-intl/server';
+import Image from 'next/image'
 import { redirect } from 'next/navigation';
 
 const SignInPage = async () => {
 
-  const t = await getTranslations('auth');
+  const t = await getTranslations();
 
   const { user } = await getAuth();
 
@@ -14,10 +15,39 @@ const SignInPage = async () => {
   }
 
   return (
-    <>
-      <h2>{t('signUp')}</h2>
-      <SignInForm />
-    </>
+    <div>
+      <div className="container relative h-screen flex-col items-center justify-center overflow-hidden md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+          <div className="absolute inset-0 bg-zinc-900">
+            <Image
+              src="/images/authentication.jpg"
+              layout="fill"
+              objectFit="cover"
+              alt="Authentication"
+            />
+          </div>
+        </div>
+        <div className="lg:p-8">
+          <div className="mx-auto flex w-full flex-col items-center justify-center space-y-6 sm:w-[450px]">
+            <Image
+              src="/logo.png"
+              width={100}
+              height={100}
+              alt="Acento Tónico"
+            />
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                {t('title')}
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                {t("auth.description")}
+              </p>
+            </div>
+            <SignInForm />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/features/auth/components/sign-in-form.tsx
+++ src/features/auth/components/sign-in-form.tsx
@@ -1,11 +1,18 @@
+import { Input } from '@/components/ui/input';
 import { signIn } from '../actions/sign-in';
+import { Button } from '@/components/ui/button';
+import { useTranslations } from 'next-intl';
 
 const SignInForm = () => {
+
+  const t = useTranslations();
+
   return (
     <form action={signIn} className="p-4 flex flex-col gap-y-2">
-      <input name="username" type="text" placeholder="Username" />
-      <input name="password" type="password" placeholder="Password" />
-      <button type="submit">Sign In</button>
+      <Input name="username" type="text" placeholder={t("labels.username")} />
+      <Input name="password" type="password" placeholder={t("labels.password")} />
+
+      <Button type="submit">{t("auth.signIn")}</Button>
     </form>
   );
 };

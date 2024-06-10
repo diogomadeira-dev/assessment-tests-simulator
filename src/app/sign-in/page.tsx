3f28179703d24c41b2cfdf2cca2e672bcc92ee48
Diogo Madeira <diogomadeira10@gmail.com>
2024-06-10 22:26:18 +0100

    add shadcn form

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -17,7 +17,7 @@ const SignInPage = async () => {
   return (
     <div>
       <div className="container relative h-screen flex-col items-center justify-center overflow-hidden md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex">
           <div className="absolute inset-0 bg-zinc-900">
             <Image
               src="/images/authentication.jpg"
